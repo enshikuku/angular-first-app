08aef9a1cb0e2524bf04eb674945b9019de949d2
Shikuku39 <enshikuku39@gmail.com>
2024-06-18 12:39:06 +0300

    chore: add HousingLocationComponent input and import statements, and update HomeComponent with sample data

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -5,16 +5,19 @@ import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-  <section class="listing">
-    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-  </section>
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    </section>
   `,
-  styleUrl: './housing-location.component.css'
+  styleUrls: ['./housing-location.component.css'],
 })
+
 export class HousingLocationComponent {
+
   @Input() housingLocation!: HousingLocation;
+
 }
