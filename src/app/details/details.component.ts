826f60165e91cbf9e55fcc29046755d441ffb245
Shikuku39 <enshikuku39@gmail.com>
2024-06-20 10:29:40 +0300

    fix: Parse housingLocationId as an integer in DetailsComponent constructor

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -56,10 +56,14 @@ export class DetailsComponent {
     lastName: new FormControl(''),
     email: new FormControl('')
   });
+  
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
   }
+
   submitApplication() {
     this.housingService.submitApplication(
       this.applyForm.value.firstName ?? '',
